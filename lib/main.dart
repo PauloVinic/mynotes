fd0db6563ac73f81ab2698485da516390743bb30
Paulo <paulo_vinicius_21@hotmail.com>
2023-09-12 15:50:38 -0300

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +20,7 @@ void main() {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
         '/verify/': (context) => const VerifyEmailView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
@@ -72,7 +72,11 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Main UI"),
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Main UI",
+          style: TextStyle(color: Colors.white),
+        ),
         actions: [
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
